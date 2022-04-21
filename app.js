ecf544935b531b5057d92e2f9b9a586f547dc3e0
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-04-21 06:07:26 -0500

    close modal

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ function Gallery(element){
   // bind function
 
   // this.openModal = this.openModal.bind(this);
+  this.closeModal = this.closeModal.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.prevImage = this.prevImage.bind(this);
   // container event
   this.container.addEventListener('click',function(e){
     // console.log(this);
@@ -44,11 +47,27 @@ Gallery.prototype.openModal = function(selectedImage,list){
     return `<img src="${image.src}" title="${image.title} data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}"/>`
   }).join('')
   this.modal.classList.add('open');
+  this.closeBtn.addEventListener('click', this.closeModal);
+  this.nextBtn.addEventListener('click', this.nextImage);
+  this.prevBtn.addEventListener('click', this.prevImage);
 }
 
 Gallery.prototype.setMainImage = function(selectedImage){
   this.modalImg.src = selectedImage.src
   this.imageName.textContent = selectedImage.title
+};
+
+Gallery.prototype.closeModal = function(){
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener('click', this.closeModal);
+  this.nextBtn.removeEventListener('click', this.nextImage);
+  this.prevBtn.removeEventListener('click', this.prevImage);
+};
+Gallery.prototype.nextImage = function(){
+
+}
+Gallery.prototype.prevImage = function(){
+  
 }
 
 const nature = new Gallery(getElement('.nature'));
